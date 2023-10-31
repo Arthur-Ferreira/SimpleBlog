a830c42f6c0febb047d33109e0536803e5c1050f
Arthur Ferreira <63617527+Arthur-Ferreira@users.noreply.github.com>
2023-10-31 10:50:13 -0300

    Readme and Layout Files update

---
+++ README.md
@@ -0,0 +1,35 @@
+<!-- // TODO: Guest users can search for many places
+// TODO: Companys can signup to collect items
+  // TODO: Authentication
+  // TODO: Siignin and signout -->
+
+<h1 align="center">
+    MyBlog
+</h1>
+<img src="public/images/layout.png" alt="My Blog Layout"/>
+
+<nav>
+  <ul style="list-style:none;display:flex;justify-content:center;gap:40px;">
+    <li>
+      <a href="#-projeto">Projeto</a>
+    </li>
+    <li>
+      <a href="#rocket-tecnologias">Tecnologias</a>
+    </li>
+  </ul>
+</nav>
+
+## 💻 Projeto
+
+
+O projeto construído é um Blog simples, onde podemos navegar, ver todas as publicações, criar novas, editar e excluí-las, desta forma teremos um CRUD (Create, Read, Update, Delete) completo.
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [Node.js](https://nodejs.org/en/)
+- [Express](https://expressjs.com/pt-br/)
+- [MySQL](https://www.mysql.com/)
+- [EJS](https://ejs.co/)
+
--- routes/blog.js
+++ routes/blog.js
@@ -1,27 +1,23 @@
-const express = require('express')
+const express = require('express');
 
-const db = require('../data/database')
+const db = require('../data/database');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', (req, res) => {
   res.redirect('/posts')
-})
+});
 
 router.get('/posts', async (req, res) => {
   const query = `
     SELECT posts.*, authors.name AS author_name FROM posts 
     INNER JOIN authors ON posts.author_id = authors.id
-  `
-  const [posts] = await db.query(query)
-  res.render('posts-list', { posts: posts })
-})
+  `;
 
+  const [posts] = await db.query(query);
 
-router.get('/new-post', async (req, res) => {
-  const [authors] = await db.query('SELECT * FROM authors')
-  res.render('create-post', { authors: authors })
-})
+  res.render('posts-list', { posts: posts });
+});
 
 
 router.post('/posts', async (req, res) => {
@@ -36,23 +32,32 @@ router.post('/posts', async (req, res) => {
   await db.query('INSERT INTO blog.posts (title, summary, body, author_id) VALUES(?)', [
     data,
   ])
+
   res.redirect('/posts')
 })
+
+
+router.get('/new-post', async (req, res) => {
+  const [authors] = await db.query('SELECT * FROM authors');
+
+  res.render('create-post', { authors: authors });
+
+});
 
 
 
 router.get('/posts/:id', async (req, res) => {
   const query = `
-  SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts
-  INNER JOIN authors ON posts.author_id = authors.id
-  WHERE posts.post_id = ?
+    SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts
+    INNER JOIN authors ON posts.author_id = authors.id
+    WHERE posts.post_id = ?
   `
   const [posts] = await db.query(query, [req.params.id])
-  
+
   if (!posts || posts.length === 0) {
     return res.status(404).render('404')
   }
-  
+
   const postData = {
     ...posts[0],
     date: posts[0].date.toISOString(),
@@ -63,50 +68,52 @@ router.get('/posts/:id', async (req, res) => {
       day: 'numeric'
     }),
   }
-  
+
   res.render('post-detail', { post: postData })
-  
+
 })
 
 router.get('/posts/:id/edit', async (req, res) => {
   const query = `
-  SELECT * FROM posts WHERE posts.post_id = ?
+    SELECT * FROM posts WHERE posts.post_id = ?
   `
+
   const [posts] = await db.query(query, [req.params.id])
-  
+
   if (!posts || posts.length === 0) {
     return res.status(404).render('404')
   }
-  
-  res.render('update-post', { post: posts[0]})
+
+  res.render('update-post', { post: posts[0] })
 })
 
 
 router.post('/posts/:id/edit', async (req, res) => {
   const query = `
-  UPDATE posts SET title = ?, summary = ?, body = ?
-  WHERE id = ?
+    UPDATE posts SET title = ?, summary = ?, body = ?
+    WHERE post_id = ?
   `
+
   const [posts] = await db.query(query, [
     req.params.title,
     req.params.summary,
     req.params.content,
     req.params.id
   ])
-  
+
   if (!posts || posts.length === 0) {
     return res.status(404).render('404')
   }
-  
+
   res.redirect('/posts')
 })
 
 router.post('/posts/:id/delete', async (req, res) => {
   const query = `
-  DELETE FROM posts WHERE post_id = ?
+    DELETE FROM posts WHERE post_id = ?
   `
   const [posts] = await db.query(query, [req.params.id])
-    
+
   res.redirect('/posts')
 })
 
